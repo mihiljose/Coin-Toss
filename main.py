f0cdf07cad603a097d50aa4e95f0833afef9140a
Mihil Jose <>
2021-04-16 04:05:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+import random
+
+coin_toss = random.randint(0, 1)
+
+if(coin_toss==1):
+  print("Heads")
+else:
+  print("Tails")
+
+
+
+
+
+
+
+
